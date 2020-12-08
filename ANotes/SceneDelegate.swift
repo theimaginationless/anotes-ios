4af1214e751d71018f91d9f71c72e3231c23fc2d
Dmitry Teplyakov <dimon2242@gmail.com>
2020-12-08 03:38:09 +0300

    Remove some debugging prints

--- ANotes/SceneDelegate.swift
+++ ANotes/SceneDelegate.swift
@@ -42,7 +42,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate, ApplicationLockBiometri
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        print(#function)
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
